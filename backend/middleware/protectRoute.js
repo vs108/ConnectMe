60f161728c9f2a8e1021a1f7977fd37da81ad403
VIKAS SINGH <113005914+vs108@users.noreply.github.com>
2025-04-22 23:20:47 +0530

    Update protectRoute.js

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -9,7 +9,7 @@ const protectRoute = async (req, res, next) => {
 			return res.status(401).json({ error: "Unauthorized - No Token Provided" });
 		}
 
-		const decoded = jwt.verify(token, DFGDSFSLFGSDFG);
+		const decoded = jwt.verify(token, "ABCD");
 
 		if (!decoded) {
 			return res.status(401).json({ error: "Unauthorized - Invalid Token" });
