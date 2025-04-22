398d3dfd7ff36a0073c1cb0feaaf50083fbf2998
VIKAS SINGH <113005914+vs108@users.noreply.github.com>
2025-04-22 19:33:47 +0530

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
+		await mongoose.connect(mongodb+srv://<db_username>:a2OzU5gsBYYlGKfE@cluster0.37thxt0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0);
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
