1273f5ee45ff15b96c221946149f0cfd7919a86c
VIKAS SINGH <113005914+vs108@users.noreply.github.com>
2025-04-22 23:15:25 +0530

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect("mongodb+srv://vikassingh25802:a2OzU5gsBYYlGKfE@cluster0.37thxt0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+		await mongoose.connect("mongodb+srv://vikassingh25802:3py0ow8HNXCgIbFA@cluster0.dcfjeow.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
